c13b35799fb3dec8c21c633e3bd4da821bedfa1d
Ahmed-Hasan-1 <67329581+Ahmed-Hasan-1@users.noreply.github.com>
2020-07-11 18:20:04 +0200

    Update config.ts

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,12 +1,12 @@
 // apiId can be found after running the service deployments using sls
-const apiId = "ujy3txnqm6";
+const apiId = "4ymq1lh86l";
 const region = "eu-west-2";
 const stage = "dev";
 export const apiEndpoint = `https://${apiId}.execute-api.${region}.amazonaws.com/${stage}`;
 
 // Auth0 Configuration
 export const authConfig = {
-  domain: "dev-h22b0uti.eu.auth0.com", // Auth0 domain
-  clientId: "a00gmtdtP4BHJHn9jL9VmeToCDWE58YZ", // Auth0 client id
-  callbackUrl: "/callback"
+  domain: "dev-p88tp9yz.us.auth0.com", // Auth0 domain
+  clientId: "RPGj18D1DMF0PDZKgXNABuHfzNrV7pDc", // Auth0 client id
+  callbackUrl: "http://localhost:3000/callback"
 };
